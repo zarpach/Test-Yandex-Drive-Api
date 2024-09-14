67d3df901ed57c227812654e2187772071f490ab
admin <here1187@outlook.com>
2024-09-14 18:48:30 +0600

    fix: improve ui and document images

--- utils/files.py
+++ utils/files.py
@@ -1,3 +1,21 @@
+import aiohttp
+import environ
+from asgiref.sync import sync_to_async, async_to_sync
+from drive.models import Resource
+from django.conf import settings
+import asyncio
+import math
+import os.path
+
+env = environ.Env()
+environ.Env.read_env()
+
+
+def get_file_extension(filename: str) -> str:
+    extension = os.path.splitext(filename)[1][1:]
+    return extension
+
+
 def convert_size(size_bytes: int) -> str:
     if size_bytes == 0:
         return "0B"
@@ -6,3 +24,51 @@ def convert_size(size_bytes: int) -> str:
     p = math.pow(1024, i)
     s = round(size_bytes / p, 2)
     return "%s %s" % (s, size_name[i])
+
+
+def validate(key, val) -> any:
+    result: any = None
+    try:
+        result = key[val]
+    except Exception as e:
+        print(e)
+
+    if val == 'size' and result is not None:
+        result = convert_size(result)
+
+    return result
+
+
+@sync_to_async
+def create_resource(data) -> any:
+    if Resource.objects.filter(resource_id__exact=data['resource_id']).count() != 0:
+        return
+
+    return Resource.objects.create(
+        resource_id=validate(data, 'resource_id'),
+        name=validate(data, 'name'),
+        file_url=validate(data, 'public_key'),
+        file_extension=get_file_extension(validate(data, 'name')),
+        preview_url=validate(data, 'preview'),
+        type=validate(data, 'type'),
+        media_type=validate(data, 'media_type'),
+        size=validate(data, 'size'),
+        created_at=validate(data, 'created'),
+        updated_at=validate(data, 'modified')
+    )
+
+
+async def parse_data(public_key: str, path: str = None, preview_crop: str = 'true'):
+    payload = {
+        'public_key': public_key,
+        'path': path,
+        'preview_crop': preview_crop
+    }
+    headers = {'Authorization': env('OAUTH2TOKEN')}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(settings.EXTERNAL_DRIVE_BASE_URL, params=payload, headers=headers) as response:
+            response_data = await response.json()
+
+            for r in response_data['_embedded']['items']:
+                await create_resource(r)
--- yandexd_api_test/settings.py
+++ yandexd_api_test/settings.py
@@ -21,7 +21,6 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -32,7 +31,6 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -79,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'yandexd_api_test.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 STATICFILES_DIRS = [
@@ -92,7 +89,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -112,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -124,7 +119,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
@@ -134,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# app consts
+EXTERNAL_DRIVE_BASE_URL = 'https://cloud-api.yandex.net/v1/disk/public/resources'
